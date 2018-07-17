07094370f0474ab2553b1e63a909ccda76cbb7ab
NguyenKhanhLien <jery.nguyen1993@gmail.com>
2018-07-17 23:44:12 +0700

    upload and display photo in div

--- my-bootstrap-app-sass/src/app/app.component.ts
+++ my-bootstrap-app-sass/src/app/app.component.ts
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  logo = '';
   //url: 'google.com';
+  onChangeSelection(fileInput) {
+    this.logo = fileInput.target.files[0];
+
+    let reader = new FileReader();
+
+    reader.onload = (e: any) => {
+        this.logo = e.target.result;
+        console.log('logo:', this.logo);
+        console.log('byID: ', document.getElementById('upload-file'));
+        console.log('byClass: ', document.getElementsByClassName('upload-image')[0]);
+        // document.getElementById('upload-file').style.backgroundImage = "url(" + this.logo + ")";
+        (<HTMLElement>document.getElementsByClassName('upload-image')[0]).style.backgroundImage = "url(" + this.logo + ")";
+    }
+
+    reader.readAsDataURL(fileInput.target.files[0]);
+  }
 }
