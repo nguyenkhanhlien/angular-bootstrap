c1a793a424ea9e97fb5a993aed3df97be43d0bd1
NguyenKhanhLien <jery.nguyen1993@gmail.com>
2018-07-17 22:26:08 +0700

    display google map

--- my-bootstrap-app-sass/src/app/app.module.ts
+++ my-bootstrap-app-sass/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ButtonsModule } from 'ngx-bootstrap';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyCIT_Ik83sIz9WdgXcpsq123zRwU2eIDZ0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
